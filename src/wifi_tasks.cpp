36c5bf4d965993ecb1ee02a3694d79fc7585d7df
Anonymous <zixi.qi@bngu.io>
2025-02-19 01:52:53 +1100

    UPDATE Wi-Fi startup logic

--- src/wifi_tasks.cpp
+++ src/wifi_tasks.cpp
@@ -5,6 +5,9 @@ static WiFiClass Wifi;
 void wifi_init(){
     Wifi.mode(WIFI_AP);
     WiFi.softAPConfig(IPAddress(10,0,0,1), IPAddress(10,0,0,1), IPAddress(255,255,255,0), IPAddress(10,0,0,100));
-    Wifi.softAP("CanWaifu", "canwaifu");
-    Serial.println("[Wifi] Wifi is up.");
+    WiFi.softAPsetHostname("CanWaifu");
+    if (Wifi.softAP("CanWaifu", "canwaifu")){
+        Serial.println("[Wifi] Wifi is up.");
+    }
+    
 }
