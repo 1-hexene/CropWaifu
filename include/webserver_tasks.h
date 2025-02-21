60bd47f9e9ad61dceb969c86f63ca52769cf8c26
哈基米 <zixi.qi@bngu.io>
2025-02-22 01:44:31 +1100

    loading screen

--- include/webserver_tasks.h
+++ include/webserver_tasks.h
@@ -3,6 +3,10 @@
 
 #define HTTP_SERVER_PORT 80
 #include <ESPAsyncWebServer.h> 
+#include <SPIFFS.h>
+#include <iostream>
+#include <fstream>
+#include <string>
 
 void webserver_init();
 void webserver_post_task(void* pvParameters);
--- src/webserver_tasks.cpp
+++ src/webserver_tasks.cpp
@@ -1,13 +1,25 @@
 #include <webserver_tasks.h>
 
+
 static AsyncWebServer webServer(HTTP_SERVER_PORT);
+File loading_html;
+String loading_html_content;
 
 void webserver_init(){
-    
+    while (!SPIFFS.begin(true))
+    {
+        Serial.println("[Webserver] SPIFFS init failed. Retrying in 3 seconds..");
+        vTaskDelay(1000/portTICK_PERIOD_MS);
+    }
+    Serial.println("[Webserver] SPIFFS OK.");
+    loading_html = SPIFFS.open("/loading.html");
+    loading_html_content = loading_html.readStringUntil('\0');
+    loading_html.close();
+
     webServer.on("/", HTTP_GET, [](AsyncWebServerRequest* request) { 
 	   Serial.print("[WebServer]: New request received:");  // for debugging 
 	   Serial.println("GET /");        // for debugging 
-	   request->send(200, "text/html", "<html><body><h1> 404 Not Found </h1></body></html>"); 
+	   request->send(200, "text/html", loading_html_content); 
     });
     webServer.begin();
     Serial.printf("[WebServer] WebServer is listening on %d...\n", 80);
