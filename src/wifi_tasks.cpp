d2104503a0eb41448f654b4f7ef1c076c9ec8aaf
胖宝宝胖宝宝小白手套AUV <zixi.qi@bngu.io>
2025-03-05 11:25:21 +0000

    更新了SSID的生成方式，每个板子SSID唯一

--- src/wifi_tasks.cpp
+++ src/wifi_tasks.cpp
@@ -2,17 +2,37 @@
 
 static WiFiClass Wifi;
 
+/* 查表法转换字符串
+ * C++我草泥马，高级语言也没有一个正常的int到char转换函数，还得自己实现，早晚换成Lua写
+*/
+static std::string fuseSnToSsid(const uint8_t* data, bool uppercase = true) {
+    const char* hex_chars = uppercase ? "0123456789ABCDEF" : "0123456789abcdef";
+    std::string ssid = "CanWaifu_";;
+    uint8_t len = sizeof(data);
+    ssid.reserve(len * 2); // 预分配空间优化性能
+    
+    for (size_t i = 0; i < len; ++i) {
+        uint8_t byte = data[i];
+        ssid.push_back(hex_chars[(byte >> 4) & 0x0F]); // 处理高4位
+        ssid.push_back(hex_chars[byte & 0x0F]);         // 处理低4位
+    }
+
+    return ssid;
+
+}
+
 canwaifu_status wifi_init(){
     Wifi.mode(WIFI_AP);
     WiFi.softAPConfig(IPAddress(10,0,0,1), IPAddress(10,0,0,1), IPAddress(255,255,255,0), IPAddress(10,0,0,100));
     WiFi.softAPsetHostname("CanWaifu");
     uint8_t fuseSerialNumber[6] = {1,1,4,5,1,4};
-    std::string ssid = "CanWaifu_";
+
     // 获取熔丝烧录的硬件唯一id
     esp_efuse_mac_get_default(fuseSerialNumber);
-    ssid.append((char*)fuseSerialNumber);
+    std::string ssid = fuseSnToSsid(fuseSerialNumber);
+    Serial.println(ssid.c_str());
 
-    if (Wifi.softAP("CanWaifu", "canwaifu")){
+    if (Wifi.softAP(ssid.c_str(), "canwaifu")){
         Serial.println("[Wifi] Wifi is up.");
         return CANWAIFU_OK;
     } else
@@ -21,3 +41,4 @@ canwaifu_status wifi_init(){
         return CANWAIFU_ERR;
     }
 }
+
