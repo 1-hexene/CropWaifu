7e6957bc6a839c4265d2782246be07e26ca111b8
Zixi Qi <zixi.qi@bngu.io>
2025-07-04 08:46:40 +0000

    test

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@ void setup()
 
   vTaskDelay(1000 / portTICK_PERIOD_MS);
   Serial.begin(115200);
-  Serial.print("CanWaifu Software Version: ");
+  Serial.print("CorpWaifu Software Version: ");
   Serial.println(SW_VER);
   // 如果初始化炸掉了 （其中有一个的结果是1）
   if (wifi_init() | timer_init())
